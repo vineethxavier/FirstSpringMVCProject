87aee7bc6f61674a09910710019ea8b0311b6ce6
Vineeth Xavier <vineethxavier89@gmail.com>
2017-07-02 16:43:54 -0500

    added howdy.jsp = modified hellocontroller.java

--- src/com/gontuseries/hellocontroller/HelloController.java
+++ src/com/gontuseries/hellocontroller/HelloController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+
+@RequestMapping("/greet")
 public class HelloController {
 
 	@RequestMapping("/welcome")
@@ -19,4 +21,18 @@ public class HelloController {
 		return model;
 	}
 
+	@RequestMapping("/hi")
+	public ModelAndView hiWorld() {
+		ModelAndView model = new ModelAndView("HelloPage ");
+		 model.addObject("msg", "hi world-> /hi");
+		 return model;
+	}
+	
+	@RequestMapping("/howdy")
+	public ModelAndView howdyWorld() {
+		ModelAndView model = new ModelAndView("howdy");
+		model.addObject("msg", "msg-> /howdy");
+		model.addObject("msg1", "msg1-> /howdy");
+		return model;
+	}
 }
